295b511cf557a5ef0959e8f886503c504f5eb476
Mark Scripter <markscript@gmail.com>
2017-08-01 20:50:50 -0500

    updated directory structure; added pug to render out the file for easier and better organization or built page.

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
-module.exports = {
+const common = {
   autoprefixer: {
     browsers: ['last 2 version']
   },
   plugins: [
     require('postcss-import')(),
-    require('postcss-cssnext')(),
-    require('cssnano')()
+    require('postcss-cssnext')()
   ]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  common.plugins.push(require('cssnano')())
+}
+
+module.exports = common
